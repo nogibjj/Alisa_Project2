858c125967ca73b7b5e6193511ed06a86c9c7160
alisa0705 <89174034+alisa0705@users.noreply.github.com>
2022-10-09 21:01:09 -0400

    Update main.sh
    First change

--- main.sh
+++ main.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo ""
+echo "Enter the year you are interested in"
+read file
+if [ -e "$file" ]
+then
+    cat $file | while read line
+    do
+        echo $line
+        if [ $line -gt 1990]
+        then
+            echo "Contemporary"
+        else
+            echo "Ancient"
+        fi
+    done
+else
+    echo "$file does not exist"
+fi
